b76d721c5b7c700c27962a8db07f239b32fa403a
Rob Langdon <roblangdon@Robs-Laptop.local>
2024-04-19 22:04:49 +0100

    Lessons up to Filters

---
+++ app/routes/application--edit-name.js
@@ -0,0 +1,48 @@
+const _ = require('lodash')
+
+
+module.exports = router => {
+
+    router.get('/applications/:applicationId/edit-name', (req, res) => {
+      let application = req.session.data.applications.find(application => application.id === req.params.applicationId)
+  
+      // Allows a null value if the user hasn't updated the field, and also falls back to use the original data
+      let firstName = _.get(req, 'session.data.editName.firstName') || application.personalDetails.firstName
+      let lastName = _.get(req, 'session.data.editName.lastName') || application.personalDetails.lastName
+
+      res.render('applications/edit-name/index', {
+        application,
+        firstName,
+        lastName
+      })
+    })
+  
+    router.post('/applications/:applicationId/edit-name', (req, res) => {
+      res.redirect(`/applications/${req.params.applicationId}/edit-name/check`)
+    })
+
+    router.get('/applications/:applicationId/edit-name/check', (req, res) => {
+      let application = req.session.data.applications.find(application => application.id === req.params.applicationId)
+
+      res.render('applications/edit-name/check', {
+        application
+      })
+    })
+
+    router.post('/applications/:applicationId/edit-name/check', (req, res) => {
+      let application = req.session.data.applications.find(application => application.id === req.params.applicationId)
+
+      // Update the application
+      application.personalDetails.firstName = req.session.data.editName.firstName
+      application.personalDetails.lastName = req.session.data.editName.lastName
+
+      // Clear temporary form data
+      delete req.session.data.editName
+
+      req.flash('success', 'Applicant name updated')
+
+      res.redirect(`/applications/${req.params.applicationId}`)
+
+    })
+    
+  }
