3036878cc5da94868565660c6591593adb82572f
Rob Langdon <roblangdon@Robs-Laptop.local>
2024-04-23 11:55:44 +0100

    Added up to Handling no results

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -1,4 +1,98 @@
+const _ = require('lodash')
+
 module.exports = router => {
+
+    router.get('/applications', (req, res) => {
+        let applications = req.session.data.applications
+        
+        let emailAddress = _.get(req.session.data.search, 'emailAddress')
+
+        if(emailAddress) {
+            applications = applications.filter(application => {
+            return application.personalDetails.emailAddress.indexOf(emailAddress) > -1
+        })
+        }
+
+        // ['Received', ...]
+        let selectedStatusFilters = _.get(req.session.data.filters, 'statuses')
+        let selectedNumberOfBallsFilters = _.get(req.session.data.filters, 'numberOfBalls')
+
+        let hasFilters = _.get(selectedStatusFilters, 'length') || _.get(selectedNumberOfBallsFilters, 'length')
+
+        let selectedFilters = {
+            categories: []
+        }
+
+        // The user has selected a status filter
+        if(hasFilters) {
+            applications = applications.filter(application => {
+                let matchesStatus = true
+                let matchesNumbersOfBalls = true
+
+                if(_.get(selectedStatusFilters, 'length')) {
+                    matchesStatus = selectedStatusFilters.includes(application.status);
+                }
+
+                if(_.get(selectedNumberOfBallsFilters, 'length')) {
+                    matchesNumbersOfBalls = selectedNumberOfBallsFilters.includes(application.experience.numberOfBalls);
+                }
+               
+                return matchesStatus && matchesNumbersOfBalls
+            })
+        }
+
+        if(_.get(selectedStatusFilters, 'length')) {
+            selectedFilters.categories.push({
+                heading: { text: 'Status'},
+                items: selectedStatusFilters.map(label => {
+                    return {
+                        text: label,
+                        href: `/applications/remove-status/${label}`
+                    }
+                })
+
+            })
+        }
+
+        if(_.get(selectedNumberOfBallsFilters, 'length')) {
+            selectedFilters.categories.push({
+                heading: { text: 'Number of balls'},
+                items: selectedNumberOfBallsFilters.map(label => {
+                    return {
+                        text: label,
+                        href: `/applications/remove-numberOfBalls/${label}`
+                    }
+                })
+
+            })
+        }
+
+        res.render('applications/index', {
+            applications,
+            selectedFilters
+        })
+    })
+    
+    router.get('/applications/clear-search', (req, res) => {
+        _.set(req, 'session.data.search.emailAddress', '')
+        res.redirect('/applications/')        
+    }) 
+
+    router.get('/applications/remove-status/:status', (req, res) => {
+        _.set(req, 'session.data.filters.statuses', _.pull(req.session.data.filters.statuses, req.params.status))
+        res.redirect('/applications/')        
+    })
+
+    router.get('/applications/remove-numberOfBalls/:numberOfBalls', (req, res) => {
+        _.set(req, 'session.data.filters.numberOfBalls', _.pull(req.session.data.filters.numberOfBalls, req.params.numberOfBalls))
+        res.redirect('/applications/')        
+    })
+
+    router.get('/applications/clear-filters', (req, res) => {        
+        _.set(req, 'session.data.filters.statuses', null)
+        _.set(req, 'session.data.filters.numberOfBalls', null)
+        res.redirect('/applications/')        
+    })
 
     router.get('/applications/:applicationId', (req, res) => {
         let application = req.session.data.applications.find(application => application.id === req.params.applicationId)
